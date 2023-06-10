99f58e4371c38e0393036af3bb802d26aae1cd85
Arnold <antaloczi.arnold@gmail.com>
2023-06-10 20:58:33 +0200

    Add state management, add custom routing, update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { AppComponent } from './app.component';
-import { HeaderModule } from './modules/header/header.module';
+import { HeaderComponent } from './components/header/header.component';
 
 describe('AppComponent', () => {
-  beforeEach(() =>
+  const initialState = { data: { path: '' } };
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [HeaderModule, RouterTestingModule],
-    })
-  );
+      declarations: [AppComponent, HeaderComponent],
+      imports: [RouterTestingModule],
+      providers: [provideMockStore({ initialState })],
+    });
+    TestBed.inject(MockStore);
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderModule } from './modules/header/header.module';
+import { DataReducer } from './store/data/data-reducers';
+import { DataEffects } from './store/data/data-effects';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({ data: DataReducer }), EffectsModule.forRoot([DataEffects])],
   providers: [],
   bootstrap: [AppComponent],
 })
